710d50fccbf453dd7b8751ca2158eac8f1a13d9d
Oğuzhan Taçtan <46655443+oguzhantctn09@users.noreply.github.com>
2021-12-27 15:21:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const db = require('./database');
-const emailValid = require('./EmailValidation')
+const emailValid = require('./EmailValidation');
+const md5 = require('md5');
+
 //cors allows to use localhost:3000 and localhost:5000 together
 app.use(cors());
 app.use(express.json())  // req.body => to access json data body
@@ -28,11 +30,12 @@ app.post("/users", async(req,res) => {
     const email = req.body.email;
     const academic = req.body.academic;
     const password = req.body.parola;
+    const cryptedPassword = md5(password);
     const validAcademic = emailValid(email, academic); 
     //newUser() database query ile insert yapmayı sağlıyor [name, email, academic] değişkenleri ile
     const newUser = await db.query(
       "INSERT INTO kullanici_liste (name, email, academic, password) VALUES ($1, $2, $3, $4) RETURNING *", 
-      [name, email, validAcademic, password]
+      [name, email, validAcademic, cryptedPassword]
     );
     //respond new user 'rows' as json (to see on postman)
     res.json(newUser.rows[0]);
